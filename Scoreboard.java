cef699aa7ee85bf070feaaa377e1e02c2a283330
nessa29 <vanessay10@nycstudents.net>
2024-09-27 16:34:19 +0000

    first commit

---
+++ Scoreboard.java
@@ -0,0 +1,32 @@
+public class Scoreboard 
+{
+    private String teamOne;
+    private String teamTwo;
+    private int teamOneScore;
+    private int teamTwoScore;
+    private String activeTeam; 
+    
+    public Scoreboard(String one, String two) 
+    {
+        teamOne = one;
+        teamTwo = two;
+        activeTeam = teamOne;   
+    }
+    public String getScore() 
+    {
+        return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
+    }
+     public String recordPlay (int score)
+     {
+        return teamOneScore++ + "-" + teamTwoScore + "-" + activeTeam;
+     }
+ 
+
+
+
+}
+
+
+
+
+    
