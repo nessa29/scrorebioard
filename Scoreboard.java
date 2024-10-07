f184cbc725d30a6acc8d1a1b9d9f53b13e1a6506
nessa29 <vanessay10@nycstudents.net>
2024-10-07 12:22:13 -0400

    Update Scoreboard.java

--- Scoreboard.java
+++ Scoreboard.java
@@ -1,26 +1,44 @@
-public class Scoreboard 
+public class Scoreboard 
 {
-    private String teamOne;
-    private String teamTwo;
-    private int teamOneScore;
-    private int teamTwoScore;
-    private String activeTeam; 
-    
-    public Scoreboard(String one, String two) 
-    {
-        teamOne = one;
-        teamTwo = two;
-        activeTeam = teamOne;   
-    }
-    public String getScore() 
-    {
-        return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
-    }
-     public String recordPlay (int score)
-     {
-        return teamOneScore++ + "-" + teamTwoScore + "-" + activeTeam;
-     }
- 
+    private String teamOne;
+    private String teamTwo;
+    private int teamOneScore;
+    private int teamTwoScore;
+    private String activeTeam; 
+    
+    public Scoreboard(String one, String two) 
+    {
+        teamOne = one;
+        teamTwo = two;
+        activeTeam = teamOne;   
+    }
+    public String getScore() 
+    {
+        return teamOneScore + "-" + teamTwoScore + "-" + activeTeam;
+    }
+     public void recordPlay (int score)
+     {  
+        if (teamOne.equals(activeTeam))
+        {
+          teamOneScore += score;
+          if(score == 0)
+          {
+            activeTeam = teamTwo;
+          }
+        } 
+        else if (teamTwo.equals(activeTeam))
+        {
+            teamTwoScore += score;
+            if(score == 0)
+            {
+                activeTeam = teamOne;
+            }
+        }
+        
+        
+
+     }
+ 
 
 
 
@@ -29,4 +47,6 @@ public class Scoreboard
 
 
 
-    
+    
+
+
